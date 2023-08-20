54d8edc882187ee0448d079c3fef1eaac7b8b7a7
Zachary Wedel <133525592+zaccmagicdev@users.noreply.github.com>
2023-08-20 13:34:38 -0500

    Update CollatzConjecture.c

--- CollatzConjecture.c
+++ CollatzConjecture.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+
+
+//by putting the asterisk back in our pointer value and using it's appropriate referral type we can get the variable inside it,
+//potentially reducing file size. C is extremely proficient at just that!
+
+int main(){
+
+    int x;
+
+    printf("Enter a number that isn't too small: \n");
+    scanf("%i", &x);
+    printf("\n\n");
+
+    int c = 0;
+    const int xOrigin = x;
+
+    do{
+        if(x % 2 == 0){
+            x = (x / 2);
+            c++;
+            printf("%i\n", x);
+        } else {
+            x = (x * 3) + 1;
+            c++;
+            printf("%i\n", x);
+        }
+    } while(x != 1);
+
+    printf("\n\n%i took %i steps to go back to one\n\n", xOrigin, c);
+    return 0;
+}
 
